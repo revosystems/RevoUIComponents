909dc63c8a57d0c1661240dbec634ad587bb2cdd
Jordi Puigdellívol <jordi.p@revo.works>
2020-03-11 16:06:48 +0100

    Working with Alert Controller

--- RevoUIComponents/ViewController.swift
+++ RevoUIComponents/ViewController.swift
@@ -23,11 +23,17 @@ class ViewController: UIViewController, ContentStatusActionDelegate {
         
         stateTableView.state = .loading(text: "Loading...")
         stateView.state      = .loading(text: "Loading2...")
-        
     }
     
     func onContentStatusAction(_ sender:UIButton){
         onAsyncButtonPressed(sender)
+    }
+    
+    @IBAction func onAlertPressed(_ sender: Any) {
+        let a = Alert(alert: "hola", "que tal", okText: "Ok", cancelText: "No") { result in
+            print(result)
+        }
+        present(a, animated: true)
     }
     
     @IBAction func onShowPinPressed(_ sender: Any) {
